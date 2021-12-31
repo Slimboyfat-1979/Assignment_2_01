e938b2e12b188be2b8ca712f5e8d295b8860daf6
Dave Barnett <slimboyfat1979@gmail.com>
2022-01-01 12:58:26 +1300

    Added functionality to populate list automatically using streamreader

--- Assignment_2_01/Form3.Designer.cs
+++ Assignment_2_01/Form3.Designer.cs
@@ -74,6 +74,7 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "Remove Selected Customer";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form3
             // 
--- Assignment_2_01/Form3.cs
+++ Assignment_2_01/Form3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,13 +22,27 @@ namespace Assignment_2_01
         }
         public Form3()
         {
+            //Figure out how to read customer only once
             InitializeComponent();
-            DisplayCustomers();
+            ReadCustomers(getController());
+            //DisplayCustomers();
         }
 
         public Form3 getForm3()
         {
             return this;
+        }
+
+        public void ReadCustomers(Controller c)
+        {
+            Customer t;
+            var reader = new StreamReader("customer.txt");
+            while (!reader.EndOfStream)
+            {
+                t = new Customer(reader.ReadLine());
+                c.customerList.Add(t);
+            }
+            DisplayCustomers();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -55,11 +70,16 @@ namespace Assignment_2_01
             Customer temp = control.customerList[selectedID];
             Form5 form5 = new Form5();
             form5.createTempCustomer(temp);
-            
+            this.Hide();
             form5.Show();
 
 
             //MessageBox.Show(Convert.ToString(selectedID));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
